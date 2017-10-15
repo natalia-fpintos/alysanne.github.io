aef5f034ff019b8ec6aa7aaafd392250ddc680df
Natalia Fernández Pintos <natalia.fpintos@gmail.com>
2017-10-15 21:52:10 +0100

    Created new pokedex page

---
+++ js/script-pokedex.js
@@ -0,0 +1,6 @@
+let pokedexItems = [['Bulbasaur', '', ''],
+                    ['Charmander', '', '']
+                    ['Squirtle', '', '']];
+
+document.getElementById("text-background-small-1").style.setProperty('color', 'rgba(207, 131, 77, 0.3)');
+document.getElementById("text-background-small-1").style.setProperty('background', 'rgba(207, 131, 77, 0.3)');
